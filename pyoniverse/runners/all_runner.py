d06e21315f01f0803b89565285d0faa70df80aef
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-14 02:07:41 +0900

    [feat] multiprocessing (#26)

--- pyoniverse/runners/all_runner.py
+++ pyoniverse/runners/all_runner.py
@@ -1,8 +1,7 @@
+import asyncio
 from pathlib import Path
-from typing import NoReturn
 
 from overrides import override
-from scrapy.crawler import CrawlerRunner
 
 from pyoniverse.runners.runner import Runner
 
@@ -10,10 +9,28 @@ from pyoniverse.runners.runner import Runner
 class AllRunner(Runner):
     @classmethod
     @override
-    def _process(cls, runner: CrawlerRunner, *args, **kwargs) -> NoReturn:
+    def run(cls, *args, **kwargs):
         spider_dir = Path("pyoniverse/spiders")
+        sub_runners = []
         for spider in spider_dir.glob("*.py"):
             if not spider.name.startswith("__"):
                 name = spider.with_suffix("").stem
-                runner.crawl(name)
                 cls.logger.info(f"Run {name}")
+                sub_runners.append(cls._run(name, *args, **kwargs))
+        asyncio.run(cls._collect(sub_runners))
+
+    @classmethod
+    async def _collect(cls, sub_runners: list):
+        await asyncio.gather(*sub_runners)
+
+    @classmethod
+    async def _run(cls, spider: str, *args, **kwargs):
+        cmd = f"python main.py --stage={kwargs['stage']} {spider}"
+        proc = await asyncio.create_subprocess_shell(
+            cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+
+        stdout, stderr = await proc.communicate()
+
+        cls.logger.info(f"[{cmd} exited with {proc.returncode}]")
+        return None
--- pyoniverse/runners/runner.py
+++ pyoniverse/runners/runner.py
@@ -1,31 +1,22 @@
 import logging
 import os
 from abc import ABCMeta, abstractmethod
-from typing import NoReturn
 
-from scrapy.crawler import CrawlerRunner
 from scrapy.settings import Settings
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
-from twisted.internet import reactor
 
 
 class Runner(metaclass=ABCMeta):
     logger = logging.getLogger("scrapy.runner")
 
     @classmethod
+    @abstractmethod
     def run(cls, *args, **kwargs):
-        settings = cls.__prepare(*args, **kwargs)
-        runner = CrawlerRunner(settings)
-        cls._process(runner, *args, **kwargs)
-        d = runner.join()
-        d.addBoth(lambda _: reactor.stop())
-        reactor.run()
-        cls.logger.info("The runner ends")
-        return
+        pass
 
     @classmethod
-    def __prepare(cls, *args, **kwargs) -> Settings:
+    def _prepare(cls, *args, **kwargs) -> Settings:
         settings = get_project_settings()
         settings["LOG_LEVEL"] = kwargs.get("loglevel", "DEBUG")
         settings["LOG_FILE"] = "debug.json"  # default log file
@@ -37,8 +28,3 @@ class Runner(metaclass=ABCMeta):
             f"Stage: {settings['STAGE']}, Log Level: {settings['LOG_LEVEL']}"
         )
         return settings
-
-    @classmethod
-    @abstractmethod
-    def _process(cls, runner: CrawlerRunner, *args, **kwargs) -> NoReturn:
-        pass
