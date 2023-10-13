02def3e2a8221549a3b71c8a78e2b1dcf6ff14e8
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-13 14:46:48 +0900

    [feat] send slack

--- tests/test_out/test_slack/test_unit.py
+++ tests/test_out/test_slack/test_unit.py
@@ -2,6 +2,7 @@ import os
 from dataclasses import asdict
 from typing import Dict
 
+import dotenv
 import pytest
 
 from pyoniverse.out.model.enum.message_enum import MessageTypeEnum
@@ -13,6 +14,11 @@ from pyoniverse.out.slack.slack import SlackSender
 
 if "tests" not in os.listdir():
     os.chdir("..")
+
+
+@pytest.fixture
+def env():
+    dotenv.load_dotenv()
 
 
 @pytest.fixture
@@ -30,7 +36,7 @@ def test_convert_to_msg(data):
     msg = Message(
         type=MessageTypeEnum.TEST,
         source="pyoniverse-crawler",
-        text=f"{MessageTypeEnum.TEST} Result: {data['summary']}",
+        text=f"{MessageTypeEnum.TEST} Result: {asdict(data['summary'])}",
         ps={k: str(asdict(v)) for k, v in data.items() if k != "summary"},
         cc=["윤영로"],
     )
@@ -40,7 +46,7 @@ def test_convert_to_msg(data):
     assert msg == res
 
 
-def test_send_slack(data: Dict[str, LogResult]):
+def test_send_slack(env, data: Dict[str, LogResult]):
     # given
     sender = Sender()
     # when
