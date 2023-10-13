4339f54756307716e7edccd3e16d82d4b39a38a9
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-13 23:50:29 +0900

    [fix] cuweb (#25)

--- pyoniverse/spiders/cuweb.py
+++ pyoniverse/spiders/cuweb.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from scrapy import FormRequest, Request, Spider
 from scrapy.exceptions import DropItem
 from scrapy.http import HtmlResponse
+from scrapy.shell import inspect_response
 from scrapy.spidermiddlewares.httperror import HttpError
 from twisted.python.failure import Failure
 
@@ -73,44 +74,39 @@ class CUWebSpider(Spider):
     def start_requests(self) -> Request:
         # Get cookies
         yield Request(
-            url=self.base_url,
-            callback=self.form,
-            cb_kwargs={"page": "1"},
-            errback=self.errback,
-            dont_filter=True,
+            url=self.base_url, callback=self.__start_request, errback=self.errback
         )
 
-    def form(self, response: HtmlResponse, **kwargs) -> Request:
+    def __start_request(self, response: HtmlResponse, **kwargs) -> Request:
         # base list
         for category in self.list_category.values():
             params = self.list_params.copy()
-            params["pageIndex"] = kwargs["page"]
+            params["pageIndex"] = "1"
             params["searchMainCategory"] = category
             params["codeParent"] = category
             yield FormRequest(
                 url=self.list_base_url,
                 formdata=params,
                 callback=self.parse_list,
-                cb_kwargs={"page": kwargs["page"]},
-                dont_filter=True,
+                cb_kwargs={"params": params},
                 errback=self.errback,
             )
         # pb list
         for category in self.pb_list_category.values():
             params = self.pb_params.copy()
-            params["pageIndex"] = kwargs["page"]
+            params["pageIndex"] = "1"
             params["searchgubun"] = category
             yield FormRequest(
                 url=self.pb_list_base_url,
                 formdata=params,
                 callback=self.parse_list,
-                cb_kwargs={"page": kwargs["page"], "event": "MONOPOLY"},
-                dont_filter=True,
+                cb_kwargs={"params": params, "event": "MONOPOLY"},
                 errback=self.errback,
             )
 
     def parse_list(self, response: HtmlResponse, **kwargs) -> Request:
         soup = BeautifulSoup(response.text, "html.parser")
+
         # id 추출
         # 1. prod_item
         if soup.select_one("a.prod_item") is not None:
@@ -140,15 +136,14 @@ class CUWebSpider(Spider):
                 dont_filter=True,
                 errback=self.errback,
             )
-
         if crawl_ids:
             # 현재 페이지에 상품이 있다면 다음 페이지로 이동. 현재 페이지에 상품이 없으면 마지막 페이지에 도달한 것이므로 종료
-            page = str(int(kwargs["page"]) + 1)
-            yield Request(
-                url=self.base_url,
-                callback=self.form,
-                cb_kwargs={"page": page},
-                dont_filter=True,
+            kwargs["params"]["pageIndex"] = str(int(kwargs["params"]["pageIndex"]) + 1)
+            yield FormRequest(
+                url=response.url,
+                callback=self.parse_list,
+                formdata=kwargs["params"],
+                cb_kwargs=kwargs,
                 errback=self.errback,
             )
 
@@ -162,76 +157,73 @@ class CUWebSpider(Spider):
         detail = soup.select_one("div.prodDetail")
         if not detail:
             self.logger.debug("Fail to load detail page: {}".format(response.url))
-            sleep(1)
             yield Request(
                 url=response.url,
                 callback=self.parse,
                 cb_kwargs=kwargs,
                 dont_filter=True,
                 errback=self.errback,
-                priority=1,
             )
-            return
+        else:
+            product_img = detail.select_one("div.prodDetail-w > img")
+            if product_img is None:
+                raise DropItem("No product image")
+            product_img = "https:" + product_img["src"]
 
-        product_img = detail.select_one("div.prodDetail-w > img")
-        if product_img is None:
-            raise DropItem("No product image")
-        product_img = "https:" + product_img["src"]
+            event_imgs = detail.select("div.prodDetail-w img")
+            for event_img in event_imgs:
+                if event_img["src"].startswith("/images"):
+                    events.append(event_img["alt"])
 
-        event_imgs = detail.select("div.prodDetail-w img")
-        for event_img in event_imgs:
-            if event_img["src"].startswith("/images"):
-                events.append(event_img["alt"])
+            product_name = detail.select_one("div.prodDetail-e > p.tit").text.strip()
 
-        product_name = detail.select_one("div.prodDetail-e > p.tit").text.strip()
+            event_tags = detail.select("div.prodDetail-e > ul.prodTag > li")
+            if event_tags:
+                event_tags = list(map(lambda x: x.text.strip(), event_tags))
+                events.extend(event_tags)
 
-        event_tags = detail.select("div.prodDetail-e > ul.prodTag > li")
-        if event_tags:
-            event_tags = list(map(lambda x: x.text.strip(), event_tags))
-            events.extend(event_tags)
+            product_info = detail.select_one("div.prodDetail-e > div.prodInfo")
+            price = product_info.select_one("dd.prodPrice > p > span")
+            if price is None:
+                raise DropItem("Price is empty")
+            price = float(re.sub(r"\D", "", price.text).strip())
 
-        product_info = detail.select_one("div.prodDetail-e > div.prodInfo")
-        price = product_info.select_one("dd.prodPrice > p > span")
-        if price is None:
-            raise DropItem("Price is empty")
-        price = float(re.sub(r"\D", "", price.text).strip())
+            descriptions = detail.select("ul.prodExplain > li")
+            if not descriptions:
+                raise DropItem("Description doesn't exist")
+            descriptions = list(map(lambda x: x.text.strip(), descriptions))
+            description = "\n".join(descriptions)
 
-        descriptions = detail.select("ul.prodExplain > li")
-        if not descriptions:
-            raise DropItem("Description doesn't exist")
-        descriptions = list(map(lambda x: x.text.strip(), descriptions))
-        description = "\n".join(descriptions)
+            raw_tags = detail.select("#taglist > li")
+            if not raw_tags:
+                raise DropItem("tag doesn't exist")
+            raw_tags = list(map(lambda x: x.text.strip(), raw_tags))
+            tags.extend(raw_tags)
 
-        raw_tags = detail.select("#taglist > li")
-        if not raw_tags:
-            raise DropItem("tag doesn't exist")
-        raw_tags = list(map(lambda x: x.text.strip(), raw_tags))
-        tags.extend(raw_tags)
+            # Build product
 
-        # Build product
-
-        product = ProductVO(
-            crawled_info=CrawledInfoVO(
-                spider=self.name,
-                id=kwargs["crawl_id"],
-                url=response.url,
-                brand=convert_brand(self.brand),
-            ),
-            name=product_name,
-            price=PriceVO(value=price, currency=convert_currency("KRW")),
-            image=ImageVO(thumb=product_img),
-            events=list(
-                map(
-                    lambda x: EventVO(
-                        brand=convert_brand(self.brand), id=convert_event(x)
-                    ),
-                    events,
-                )
-            ),
-            description=description,
-            tags=tags,
-        )
-        yield product
+            product = ProductVO(
+                crawled_info=CrawledInfoVO(
+                    spider=self.name,
+                    id=kwargs["crawl_id"],
+                    url=response.url,
+                    brand=convert_brand(self.brand),
+                ),
+                name=product_name,
+                price=PriceVO(value=price, currency=convert_currency("KRW")),
+                image=ImageVO(thumb=product_img),
+                events=list(
+                    map(
+                        lambda x: EventVO(
+                            brand=convert_brand(self.brand), id=convert_event(x)
+                        ),
+                        events,
+                    )
+                ),
+                description=description,
+                tags=tags,
+            )
+            yield product
 
     def errback(self, failure: Failure):
         # Retry if 403 Forbidden
@@ -242,7 +234,7 @@ class CUWebSpider(Spider):
             sleep(5)
             yield Request(
                 self.base_url,
-                callback=self.form,
+                callback=self.__start_request,
                 cb_kwargs=req.cb_kwargs,
                 dont_filter=True,
             )
