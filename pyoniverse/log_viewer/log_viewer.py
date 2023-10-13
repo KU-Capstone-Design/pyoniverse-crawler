e515a605cdb551d89273955d365197b6c9f10c00
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-13 12:15:03 +0900

    [feat] log result summary

--- pyoniverse/log_viewer/log_viewer.py
+++ pyoniverse/log_viewer/log_viewer.py
@@ -2,7 +2,6 @@ import os
 import re
 from pathlib import Path
 from typing import Dict, Optional
-import datetime
 
 from pyoniverse.log_viewer.model.log_result import LogResult
 
@@ -17,11 +16,13 @@ class LogViewer:
             if not f.endswith(".log"):
                 continue
             log_name = f[:-4]  # .log 제외
-            log_result = self._parse(Path(root_dir) / f)
-            res[log_name] = log_result
+            if log_result := self._parse(Path(root_dir) / f):
+                res[log_name] = self._convert(log_result)
+
+        res["summary"] = self._summary(res)
         return res
 
-    def _parse(self, log_path: Path) -> Optional[LogResult]:
+    def _parse(self, log_path: Path) -> Optional[dict]:
         with open(log_path, "r") as fd:
             context = fd.read()
             stats = re.search(r"Dumping Scrapy stats:\n(\{.+\})", context, re.DOTALL)
@@ -30,3 +31,25 @@ class LogViewer:
             else:
                 res = eval(stats.group(1))
                 return res
+
+    def _convert(self, data: dict) -> LogResult:
+        res = LogResult(
+            collected_count=data["item_scraped_count"],
+            error_count=data["log_count/ERROR"],
+            elapsed_sec=int(data["elapsed_time_seconds"]),
+        )
+        return res
+
+    def _summary(self, res: Dict[str, LogResult]) -> LogResult:
+        collected_count = 0
+        error_count = 0
+        elapsed_sec = 0
+        for v in res.values():
+            collected_count += v.collected_count
+            error_count += v.error_count
+            elapsed_sec = max(elapsed_sec, v.elapsed_sec)
+        return LogResult(
+            collected_count=collected_count,
+            error_count=error_count,
+            elapsed_sec=elapsed_sec,
+        )
--- tests/log_viewer/unittest.py
+++ tests/log_viewer/unittest.py
@@ -1,34 +1,18 @@
 import datetime
 import os
+from functools import reduce
 from pathlib import Path
 from typing import Dict
+
+import pytest
 
 from pyoniverse.log_viewer.log_viewer import LogViewer
 from pyoniverse.log_viewer.model.log_result import LogResult
 
 
-def test_log_result():
-    # given
-    result_data = {
-        "collected_count": 1,
-        "error_count": 1,
-        "elapsed_sec": 10,
-        "some_bad_field": "bad",
-    }
-    # when
-    log_result = LogResult.load(result_data)
-
-    # then
-    assert log_result.collected_count == result_data["collected_count"]
-    assert log_result.error_count == result_data["error_count"]
-    assert log_result.elapsed_sec == result_data["elapsed_sec"]
-
-
-def test_log_viewer_parse():
-    # given
-    log_viewer = LogViewer()
-    log_path = Path("./mock/mock.log")
-    parsed_result = {
+@pytest.fixture
+def result_data():
+    return {
         "downloader/request_bytes": 21030,
         "downloader/request_count": 44,
         "downloader/request_method_count/GET": 43,
@@ -58,11 +42,80 @@ def test_log_viewer_parse():
         "spider_exceptions/AttributeError": 1,
         "start_time": datetime.datetime(2023, 10, 13, 2, 41, 47, 353745),
     }
+
+
+@pytest.fixture
+def log_results():
+    return {
+        "1": LogResult(collected_count=1, error_count=1, elapsed_sec=3),
+        "2": LogResult(collected_count=3, error_count=2, elapsed_sec=1),
+        "3": LogResult(collected_count=2, error_count=0, elapsed_sec=2),
+        "4": LogResult(collected_count=5, error_count=3, elapsed_sec=5),
+    }
+
+
+def test_log_result():
+    # given
+    result_data = {
+        "collected_count": 1,
+        "error_count": 1,
+        "elapsed_sec": 10,
+        "some_bad_field": "bad",
+    }
+    # when
+    log_result = LogResult.load(result_data)
+
+    # then
+    assert log_result.collected_count == result_data["collected_count"]
+    assert log_result.error_count == result_data["error_count"]
+    assert log_result.elapsed_sec == result_data["elapsed_sec"]
+
+
+def test_log_viewer_parse(result_data):
+    # given
+    log_viewer = LogViewer()
+    log_path = Path("./mock/mock.log")
     # when
     res = log_viewer._parse(log_path)
 
     # then
-    assert res == parsed_result
+    assert res == result_data
+
+
+def test_log_viewer_convert(result_data):
+    # given
+    log_viewer = LogViewer()
+    error_count = result_data["log_count/ERROR"]
+    collected_count = result_data["item_scraped_count"]
+    elapsed_sec = int(result_data["elapsed_time_seconds"])
+
+    # when
+    res = log_viewer._convert(result_data)
+
+    # then
+    assert res.collected_count == collected_count
+    assert res.error_count == error_count
+    assert res.elapsed_sec == elapsed_sec
+
+
+def test_log_viewer_summary(log_results):
+    # given
+    log_viewer = LogViewer()
+    collected_count = reduce(
+        lambda acc, cur: acc + cur.collected_count, log_results.values(), 0
+    )
+    error_count = reduce(
+        lambda acc, cur: acc + cur.error_count, log_results.values(), 0
+    )
+    elapsed_sec = max(log_results.values(), key=lambda x: x.elapsed_sec).elapsed_sec
+
+    # when
+    res = log_viewer._summary(log_results)
+
+    # then
+    assert res.collected_count == collected_count
+    assert res.error_count == error_count
+    assert res.elapsed_sec == elapsed_sec
 
 
 def test_log_viewer():
@@ -83,7 +136,7 @@ def test_log_viewer():
     for key, val in res.items():
         assert val.collected_count >= 100
         assert val.error_count <= 10
-        assert val.elapsed_sec <= 3600
+        assert val.elapsed_sec <= 7200
     assert "summary" in res
     assert res["summary"].collected_count >= 5000
     assert res["summary"].total_error <= 50
