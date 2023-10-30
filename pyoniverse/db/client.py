8069ed39928129b58d693cd31679d6404c1b76bf
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-30 15:39:16 +0900

    [fix] db connection

--- pyoniverse/db/client.py
+++ pyoniverse/db/client.py
@@ -1,7 +1,9 @@
+import logging
 import os
 from time import sleep
 
 from pymongo import MongoClient, WriteConcern
+from pymongo.errors import ConfigurationError
 
 
 class DBClient:
@@ -38,12 +40,18 @@ class DBClient:
         self.__client = MongoClient(kwargs.get("URI", os.getenv("MONGO_URI")))
         self.__db = kwargs.get("DB", os.getenv("MONGO_DB"))
 
+        try:
+            self.__client.admin.command("ping")
+        except ConfigurationError:
+            logging.error("Cannot connect to client")
+            exit(1)
+
     def clear(self):
         """
         DB의 모든 컬렉션 데이터 제거
         """
         write_db = self.__client.get_database(
-            self.__db, write_concern=WriteConcern(w="majority", wtimeout=30)
+            self.__db, write_concern=WriteConcern(w="majority", wtimeout=60)
         )
         for col in write_db.list_collection_names():
             write_db.get_collection(col).delete_many({})
